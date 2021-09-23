5a74109b64bdd25226a547c35ea799e76b52a5bd
Damian Pazos <damian_ariel_pazos>
2021-09-23 13:55:37 -0300

    Terminado

--- Entrega N°3(5) - Pazos Damian.py
+++ Entrega N°3(5) - Pazos Damian.py
@@ -13,18 +13,18 @@ numeros = [1, 3, 6, 9]
 # Se pide al usuario el ingreso del numero
 numero_ingresado = int(input("Ingrese un numero del 1 al 9: "))
 
-# Se ingresa al while con un true
+# Se ingresa al while con la condicion necesaria para que se repita el procedimiento
+# Se se ingreso un numero incorrecto entra al while
 while 0 > numero_ingresado or numero_ingresado > 9:
-    print("Ingreso un numero incorrecto")
-    numero_ingresado = int(input("Ingrese un numero del 1 al 9: "))    
+    print("Ingreso un numero incorrecto") # Se devuelve una impresion que demuestra que se ingreso un numero incorrecto
+    numero_ingresado = int(input("Ingrese un numero del 1 al 9: ")) # Se pide que ingrese el numero nuevamente
+# Si se ingreso correctamente entra en el else
 else :
-    for cont,a in enumerate(numeros):
-        print(len(numeros), cont)
-        
-        if a == numero_ingresado:
-            print("El numero se encuentra en la lista")
-            break
-        if len(numeros) == cont:
-            print("El numero no se encuentra en la lista")
-            break
-
+    # Utilizamos un for para recorrer la lista dada
+    for cont,a in enumerate(numeros): # Utilizo enumerate y un contador para verificar si no se selecciono ningun numero de la lista         
+        if a == numero_ingresado: # Si un numero esta en la lista ingresa al if
+            print("El numero se encuentra en la lista") # Se devuelve que el valor esta en la lista
+            break # Se utiliza para salir del for
+        if (len(numeros)-1) == cont: # Si el contador se iguala a la cantidad de numeros de la lista quiere decir que no entro al if anterior por lo que ningun numero esta en la lista
+            print("El numero no se encuentra en la lista") # Se devuelve que el numero no esta en la lista
+            break # Se utiliza para salir del for
