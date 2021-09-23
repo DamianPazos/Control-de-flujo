ee2409debbd1f6ff67c80385a7cce5576490e8f5
Damian Pazos <damian_ariel_pazos>
2021-09-23 19:40:09 -0300

    En desarrollo

---
+++ Entrega N°3(7) - Pazos Damian.py
@@ -0,0 +1,17 @@
+'''
+Dadas dos listas, 
+debes generar una tercera con todos los elementos que se repitan en ellas, 
+pero no debe repetirse ningún elemento en la nueva lista
+
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+'''
+
+# Declaro las listas ya dadas
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+
+# Declaro la tercer lista donde iran todos los elementos repetidos
+lista_3 = []
+
+# 
