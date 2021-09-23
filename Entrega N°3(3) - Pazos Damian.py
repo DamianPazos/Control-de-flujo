6dc95ea80093f354fc168f8dae7813d2f96b32c9
Damian Pazos <damian_ariel_pazos>
2021-09-23 03:04:32 -0300

    Terminado

---
+++ Entrega N°3(3) - Pazos Damian.py
@@ -0,0 +1,11 @@
+'''
+Escribí un programa que sume todos los números enteros impares desde el 0 hasta el 100
+'''
+
+# Para la suma genero una variable
+suma = 0
+
+# Utilizo un range para seleccionar el rango de numeros a sumar
+# Utilizo sum y list para que se genere una lista y se sume automaticamente
+suma = sum(list(range(1,101,2)))
+print(suma) # Imprimo el resultado
